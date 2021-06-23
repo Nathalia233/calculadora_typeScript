424a18ce97363fdc6703d6a97f98fb9466108fc8
Nathalia <System;.nathy@hotmail.com>
2021-06-22 22:43:34 -0400

    calculadora

---
+++ assets/scripts/calculadora/tela.js
@@ -0,0 +1,12 @@
+export default class tela {
+    constructor(elemento = document.querySelector("#values")) {
+        this.elemento = elemento;
+    }
+    set conteudo(valor) {
+        if (this.elemento) {
+        }
+    }
+    get conteudo() {
+    }
+}
+//# sourceMappingURL=tela.js.map
---
+++ assets/typescript/calculadora/tela.ts
@@ -0,0 +1,17 @@
+export default class tela {
+    constructor(
+        private elemento: HTMLInputElement | null = document.querySelector("#values")
+        ){
+
+        }
+
+        set conteudo(valor:string ) {
+            if(this.elemento){
+
+            }
+        }
+
+        get conteudo(): string{
+
+        }
+}
